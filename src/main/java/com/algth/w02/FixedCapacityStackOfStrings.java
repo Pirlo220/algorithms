c3b1890f3fb9056b2e39895b48c2a347eeceb056
sergio <sgalvezsoler@gmail.com>
2017-11-07 23:20:27 +0100

    Created stack with an array

---
+++ src/main/java/com/algth/w02/FixedCapacityStackOfStrings.java
@@ -0,0 +1,26 @@
+package main.java.com.algth.w02;
+
+public class FixedCapacityStackOfStrings {
+	private String[] s;
+	private int n = 0;
+	
+	public FixedCapacityStackOfStrings(int capacity) {
+		s = new String[capacity];
+	}
+	
+	public boolean isEmpty() {
+		return n == 0;
+	}
+	
+	public void push(String item) {
+		// use to index into array
+		// then increment n
+		s[n++] = item;
+	}
+	
+	public String pop() {
+		// decrement n
+		// then use to index into array
+		return s[--n];
+	}
+}
