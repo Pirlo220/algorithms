41c2f6e66672f0adce369563de15aaf0fbf35458
sergio <sgalvezsoler@gmail.com>
2017-11-12 18:14:05 +0100

    Added ResizingArrayStackOfStrings

---
+++ src/main/java/com/algth/w02/ResizingArrayStackOfStrings.java
@@ -0,0 +1,25 @@
+package main.java.com.algth.w02;
+
+public class ResizingArrayStackOfStrings {
+	private String[] s;
+	private int N;
+
+	public ResizingArrayStackOfStrings() {
+		s = new String[1];
+	}
+	
+	public void push(String item){
+		if(N == s.length) {
+			resize(2 * s.length);
+		}
+		s[N++] = item;
+	}
+	
+	private void resize(int capacity) {
+		String[] copy = new String[capacity];
+		for (int i = 0; i < N; i++) {
+			copy[i] = s[i];
+		}
+		s = copy;
+	}
+}
