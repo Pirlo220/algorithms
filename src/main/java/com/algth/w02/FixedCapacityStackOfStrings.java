cb1de783f7506b5096353a4353559b9105baa885
Sergio Galvez <Pirlo220@users.noreply.github.com>
2017-11-13 17:39:14 +0100

    Changed String type to Item type

--- src/main/java/com/algth/w02/FixedCapacityStackOfStrings.java
+++ src/main/java/com/algth/w02/FixedCapacityStackOfStrings.java
@@ -1,27 +1,27 @@
 package main.java.com.algth.w02;
 
-public class FixedCapacityStackOfStrings {
-	private String[] s;
+public class FixedCapacityStackOfStrings<Item> {
+	private Item[] s;
 	private int n = 0;
 	
 	public FixedCapacityStackOfStrings(int capacity) {
-		s = new String[capacity];
+		s = new Item[capacity];
 	}
 	
 	public boolean isEmpty() {
 		return n == 0;
 	}
 	
-	public void push(String item) {
+	public void push(Item item) {
 		// use to index into array
 		// then increment n
 		s[n++] = item;
 	}
 	
-	public String pop() {
+	public Item pop() {
 		// decrement n
 		// then use to index into array
-		String node =  s[--n];
+		Item node =  s[--n];
 		s[n] = null;
 		return node;
 	}
